005a268cdf2fc894c1feaaaaee44e8be84e309fd
Gilbert ARMENGAUD <gilbert.armengaud@gmail.com>
2015-01-18 17:24:57 +0100

    DÉBUT INTÉGRATION DE ZFC-USER, ZFC-USER-DOCTRINE, ZFC-ADMIN ET BJY-AUTHORIZE

---
+++ module/Admin/config/module.config.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Admin;
+
+return array(
+    'controllers' => array(
+        'invokables' => array(
+            'Admin\Controller\Index' => 'Admin\Controller\IndexController'
+        ),
+    ),
+    'view_manager' => array(
+        'template_path_stack' => array(
+            __DIR__ . '/../view',
+        ),
+    ),
+    // route surchargée de ZFC-Admin
+    'router' => array(
+        'routes' => array(
+            'zfcadmin' => array(
+                'type'    => 'Literal',
+                'options' => array(
+                    'route'    => '/backend',
+                    'defaults' => array(
+                        '__NAMESPACE__' => 'Admin\Controller',
+                        'controller'    => 'Index',
+                        'action'        => 'index',
+                    ),
+                ),
+                'may_terminate' => true,
+                'child_routes' => array(
+                    'default' => array(
+                        'type'    => 'Segment',
+                        'options' => array(
+                            'route'    => '/[:controller[/:action]]',
+                            'constraints' => array(
+                                'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
+                            ),
+                            'defaults' => array(
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+        ),
+    ),
+);
